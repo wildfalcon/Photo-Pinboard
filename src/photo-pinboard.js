8ab1078be525c52a284616462619352d9326ad50
Laurie Young <laurie@wildfalcon.com>
2011-06-06 14:46:43 +0100

    Load images into browser by dragging and dropping them from the file system

--- src/photo-pinboard.js
+++ src/photo-pinboard.js
@@ -1,14 +1,42 @@
-var PhotoPinboard = (function () {
+// NOTES
+// The File api at http://www.w3.org/TR/FileAPI/ 
 
-  function PhotoPinboard (name) {
-    this.name = name;
-  }
+window.addEventListener("load", function(evt){
   
-  PhotoPinboard.prototype = {
-    title: function () {
-      return 'Mr. ' + this.name;
+  //
+  // Need to turn off default dragover behaviour to get
+  // filedropping to work
+  //
+  document.addEventListener('dragover', function (evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+  }, false);
+
+  document.addEventListener('drop', function (evt) {
+    var fileReader = new FileReader();
+    var x = evt.clientX;
+    var y = evt.clientY;
+    
+    fileReader.onloadend = function(evt){
+      if (!evt.target.error) {
+        var image = new Image();
+        image.onload = function () {
+          var $self = $(this);
+          $(this).attr('width', 200).appendTo('#images');          
+        }
+        image.src = evt.target.result;
+      }
+    };
+    
+    fileReader.onerror = function(evt){
+      console.log("Error", evt)
     }
-  }
+    
+    fileReader.readAsDataURL(evt.dataTransfer.files[0]);
+    
+    evt.stopPropagation();
+    evt.preventDefault();
+  }, false)
   
-  return PhotoPinboard;
-})();
+ 
+})
